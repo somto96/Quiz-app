6e8a8bb6c04605c6759dc62a843107bd1dbb1dc6
Somto96 <motivatedsomt@gmail.com>
2019-09-26 16:37:59 +0100

    first commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,128 @@
+<template>
+  <div id="question-box-container">
+    <b-jumbotron>
+      <template slot="lead">{{currentQuestion.question}}</template>
+
+      <hr class="my-4" />
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click.prevent="selectAnswer(index)"
+          :class="answerClass(index)"
+        >{{answer}}</b-list-group-item>
+      </b-list-group>
+
+      <b-button 
+        variant="primary" 
+        @click="submitAnswers"
+        :disabled="selectedIndex === null || answered"
+      >
+        Submit
+      </b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+import _ from "lodash";
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+    increment: Function
+  },
+  data() {
+    return {
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
+    };
+  },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null;
+        this.answered = false
+        this.shuffleAnswers();
+      }
+    }
+  },
+
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+      // console.log(index)
+    },
+    // Submit function
+    submitAnswers() {
+      let isCorrect = false;
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true;
+      }
+      this.answered = true;
+      this.increment(isCorrect);
+    },
+    shuffleAnswers() {
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer
+      ];
+      this.shuffledAnswers = _.shuffle(answers);
+      this.correctIndex = this.shuffledAnswers.indexOf(
+        this.currentQuestion.correct_answer
+      );
+    },
+    answerClass(index){
+      let answerClass = ""
+
+      if(!this.answered && this.selectedIndex === index){
+        answerClass = "selected"
+      }else if(this.answered && this.correctIndex === index){
+        answerClass = "correct"
+      }else if(this.answered && this.selectedIndex === index && this.correctIndex !== index){
+        answerClass = "incorrect"
+      }
+      return answerClass;
+    }
+  }
+  // mounted: {
+
+  // }
+};
+</script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.list-group-item:hover {
+  background-color: #eee;
+  cursor: pointer;
+}
+.selected {
+  background-color: #007bff;
+  color: #fff;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: rgb(214, 61, 61);
+}
+.btn {
+  margin: 0 5px;
+}
+</style>
+
+
